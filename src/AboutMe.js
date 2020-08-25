3ea8d155b27b0e63aa6022ed04e8186f6ad5bd9d
Zdravko Mavkov <mavkov@hotmail.com>
2020-08-25 14:10:47 +0200

    Working on Contact Me component

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import './AboutMe.css'
 
 function AboutMe() {
@@ -11,6 +12,10 @@ function AboutMe() {
 		<div className='About-me'>
 			<div className='About-me-container'>
 				<h2 className='About-me-h2'>About Me</h2>
+				<p>Hi. I am a Software Engineer that focuses on Front-end web development. Most of my work is focused on
+				creating responsive websites by using HTML, CSS and JavaScript and web apps by using React.
+				The best way to see what I do is to check out my <Link to='/projects' className='About-me-projects-link'>Projects</Link>.</p>
+				<h2 className='About-me-h2'>Personality</h2>
 				<p>I am a very open, communicative and friendly person and I enjoy just such work
 				environments. I love working as part of a team and I value hard work and honesty. My
 				greatest asset is that I enjoy learning new things and do so whenever I can. Because of
@@ -19,11 +24,19 @@ function AboutMe() {
 				<h2 className='About-me-h2'>Education & Experience</h2>
 				<p>I have graduated from the Faculty of Informatics at the European University in Skopje
 				as a Software Engineer with a 9.0 GPA. Initially I worked as a hardware specialist and sales at PC
-				Discount - Skopje. After a few months I got promoted to store manager. Once I decided to transition to software development I started to
+				Discount - Skopje. Later I got promoted to store manager. Once I decided to transition to software development I started to
 				intensely work on my craft and applying for internship positions. As an intern in the
 				company Sourcico I gained knowledge and experience in the JavaEE world. Later I
 				received training as a full stack developer in the company Sorsix. Now my goal is solely
 				on finding a job as a front end developer with a focus on the React JavaScript library.</p>
+				{/* <h2 className='About-me-h2'>Additional Knowledge</h2>
+				<p>From my first encounter with computers I have been fascinated by them. I’ve come in
+				contact with, studied and worked with many programming languages, computer
+				programs and development systems. Here is a list of some of them:<br />
+				C++, Java, C#, Visual Basic, Pascal, TypeScript, Angular, Git, Bootstrap 4, Heroku,
+				Spring Boot, JavaServer Faces, Maven, Gradle, JUnit, Servlets, JSP, JSTL, JDBC,
+				MySQL, Postgresql, JAX-RS, RESTful APIs, Jersey, Intellij IDEA, Eclipse IDE, NetBeans
+				IDE, VisualStudio Code and more.</p> */}
 			</div>
 		</div>
 	)
--- src/ContactMe.js
+++ src/ContactMe.js
@@ -1,15 +1,30 @@
-import React, { useEffect } from 'react'
-// import './ContactMe.css'
+import React, { useEffect, useState } from 'react'
+import './ContactMe.css'
+import eposta from './images/e-posta18.png'
 
 function ContactMe() {
 	useEffect(() => {
-        // Update the document title
-        document.title = `Zdravko Mavkov - Contect me`
-    }, [])
+		// Update the document title
+		document.title = `Zdravko Mavkov - Contect me`
+	}, [])
+
+	const [mailShown, setMailShown] = useState(false)
+
+	const handleShowMail = () => {
+		setMailShown(true)
+	}
 
 	return (
-		<div className='certifications'>
-			Contact Me Component
+		<div className='Contact-me'>
+			<div className='Contact-me-container'>
+				<div className='Contact-me-greeting'>Feel free to contact me about any additional information at</div>
+				{mailShown !== true ? <div className='Contact-me-spam-protection'></div> : <img src={eposta} alt='' className='Contact-me-eposta'/>}
+				<div className='Contact-me-links'>
+					<i className='Contact-me-link fas fa-envelope fa-5x' onClick={handleShowMail}></i>
+					<i className='Contact-me-link fab fa-linkedin fa-5x'></i>
+					<i className='Contact-me-link fab fa-github fa-5x'></i>
+				</div>
+			</div>
 		</div>
 	)
 }
