27d7b1c7b6c8d12c3812baf60d2548b31cf36767
Zdravko Mavkov <mavkov@hotmail.com>
2020-08-27 09:26:46 +0200

    Finished the Project component. Working on Projects

--- src/Project.js
+++ src/Project.js
@@ -2,22 +2,25 @@ import React from 'react'
 import './Project.css'
 
 const Project = props => {
-    console.log('props: ', props)
-
-    const { name, projectImage, codeImage, projectWebSite, projectCodeRepo, description } = props
-
-    console.log('name: ', name)
-    console.log('projectImage: ', projectImage)
+    const { name, projectImage, projectWebSite, projectCodeRepoLink, projectDescription, codeTaskDescription, codeIcon } = props
+    // console.log('props: ', props)
 
     return (
         <div className='Project'>
-            <div className='Project-left-box'>
-                <a href={projectWebSite} target='_blank' rel='noopener noreferrer'><img className='Project-site-image' src={projectImage} alt={`${name} site`} /></a>
-                <div className='Project-name'>{name}</div>
-            </div>
+            <a  className='Project-site-link' href={projectWebSite} target='_blank' rel='noopener noreferrer'>
+                <div className='Project-left-box'>
+                    <img className='Project-site-image' src={projectImage} alt={`${name} site`} />
+                    <div className='Project-name'>{name}</div>
+                </div>
+            </a>
             <div className='Project-right-box'>
-                <div className='Project-description'>{description}</div>
-                <a href={projectCodeRepo} target='_blank' rel='noopener noreferrer'><i className='Project-code-link fab fa-github fa-5x'></i></a>
+                <div className='Project-description'>{projectDescription}</div>
+                <div className='Project-code-box'>
+                    <a className='Project-code-link' href={projectCodeRepoLink} target='_blank' rel='noopener noreferrer'>
+                        {codeIcon}
+                        <span>{codeTaskDescription}</span>
+                    </a>
+                </div>
             </div>
         </div>
     )
--- src/Projects.js
+++ src/Projects.js
@@ -14,20 +14,19 @@ const Projects = props => {
 		<div className='Projects'>
 			<div className='Projects-container'>
 				<h2 className='Projects-h2'>React projects</h2>
+				<h3 className='Projects-h3'>These projects, designed and developed by me, are a showcase of my skill.</h3>
 				<Project
 					name='Book Journal'
 					projectImage={bookJournalImage}
 					projectWebSite='https://book-journal-zm.netlify.app/'
-					projectCodeRepo='https://github.com/Shizuri/React-book-journal'
-					description={<div>
-						The Book Journal application is a tool for cataloging your books. All of the data is provided by the public Google Books API. Once you find your books you can store them in your Journal. There you can note the start and finish date, rate the book and review your experience.
-					<br />
-					<br />
-					This React single page application was developed by using Functional Components, Hooks, the Context API and React Router.
-					<br />
-					<br />
-					All data is kept in localStorage.
+					projectCodeRepoLink='https://github.com/Shizuri/React-book-journal'
+					projectDescription={<div>
+						<em>Technology:</em> This React single page application (SPA) was developed by using <strong>Functional Components, Hooks, the Context API, and React Router</strong>. All data is provided by the public Google Books API. User data is kept in localStorage.
+						<br /><br />
+						<em>Description:</em> The Book Journal application is a tool for cataloging your books. Once you find your books you can store them in your Journal. There you can note the start and finish date, rate the book, and review your experience.
 					</div>}
+					codeTaskDescription='Code Repository'
+					codeIcon={<i className='Project-code-icon fab fa-github fa-1x'></i>}
 				/>
 
 				<Project
@@ -35,7 +34,9 @@ const Projects = props => {
 					projectImage={bookJournalImage}
 					projectWebSite='https://book-journal-zm.netlify.app/'
 					projectCodeRepo='https://github.com/Shizuri/React-book-journal'
-					description='Descrtiption Blah blah blah description'
+					projectDescription='Descrtiption Blah blah blah description'
+					codeTaskDescription='code repository'
+					codeIcon={<i className='Project-code-icon fab fa-free-code-camp'></i>}
 				/>
 
 				<Project
@@ -43,7 +44,8 @@ const Projects = props => {
 					projectImage={bookJournalImage}
 					projectWebSite='https://book-journal-zm.netlify.app/'
 					projectCodeRepo='https://github.com/Shizuri/React-book-journal'
-					description='Descrtiption Blah blah blah description'
+					projectDescription='Descrtiption Blah blah blah description'
+					codeTaskDescription='code repository'
 				/>
 			</div>
 		</div>
